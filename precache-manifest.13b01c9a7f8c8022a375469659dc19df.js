d60e490a5d6eddd51843c0f0112fbf54bea48fac
GustiCahya <gusticahyax@yahoo.com>
2020-04-22 09:59:56 +0800

    Updates

---
+++ precache-manifest.13b01c9a7f8c8022a375469659dc19df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbeb80c0455f0f75efaeecdf3d5b99ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "66ef8bd283be70f0d55a",
+    "url": "/monsters-rolodex/static/css/main.51b9d19b.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ef8bd283be70f0d55a",
+    "url": "/monsters-rolodex/static/js/main.9cd74c05.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
